a7dadf231d8cdf0079ca95b35f18cc37c5005289
Matteo Conti <contimatteo1505@gmail.com>
2024-01-22 18:48:30 +0100

    WIP

--- install.sh
+++ install.sh
@@ -3,4 +3,8 @@
 pip install -U pip wheel
 pip install torch torchvision --index-url https://download.pytorch.org/whl/cu118
 pip install ninja
-pip install -r requirements.txt
+pip install -r requirements-custom.txt
+pip install "git+https://github.com/ashawkey/envlight.git"
+pip install "git+https://github.com/KAIR-BAIR/nerfacc.git@v0.5.2"
+pip install "git+https://github.com/NVlabs/nvdiffrast.git"
+pip install -v "git+https://github.com/NVlabs/tiny-cuda-nn/#subdirectory=bindings/torch"
