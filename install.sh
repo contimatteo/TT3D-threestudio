2199a1615960a9fa4b53c54c6581f0dc4858fca6
Matteo Conti <contimatteo1505@gmail.com>
2024-01-22 19:26:13 +0100

    WIP

--- install.sh
+++ install.sh
@@ -7,4 +7,6 @@ pip install -r requirements-custom.txt
 pip install "git+https://github.com/ashawkey/envlight.git"
 pip install "git+https://github.com/KAIR-BAIR/nerfacc.git@v0.5.2"
 pip install "git+https://github.com/NVlabs/nvdiffrast.git"
+# export PATH="/usr/local/cuda-11.8/bin:$PATH"
+# export LD_LIBRARY_PATH="/usr/local/cuda-11.8/lib64:$LD_LIBRARY_PATH"
 pip install -v "git+https://github.com/NVlabs/tiny-cuda-nn/#subdirectory=bindings/torch"
