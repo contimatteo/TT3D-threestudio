d7fb953f6a9f9191dd1668103b5cf2b95c0d9f67
Matteo Conti <contimatteo1505@gmail.com>
2024-01-22 19:58:31 +0100

    WIP

--- run.sh
+++ run.sh
@@ -2,4 +2,4 @@
 
 exit 0
 
-CUDA_VISIBLE_DEVICES=3 python3 launch.py --config configs/dreamfusion-sd.yaml --train --gpu 0 system.prompt_processor.prompt="$prompt" trainer.max_steps=100 system.prompt_processor.spawn=false
+CUDA_VISIBLE_DEVICES=3 python3 launch.py --config configs/dreamfusion-sd.yaml --train --gpu 0 system.prompt_processor.prompt="a shark" trainer.max_steps=100 system.prompt_processor.spawn=false
