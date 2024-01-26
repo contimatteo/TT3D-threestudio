58cc8a705c0b096813190c1d292aaeae4920112a
Matteo Conti <contimatteo1505@gmail.com>
2024-01-26 14:45:40 +0100

    WIP

--- run.sh
+++ run.sh
@@ -15,3 +15,18 @@ CUDA_VISIBLE_DEVICES=3 python3 tt3d_generate.py \
 CUDA_VISIBLE_DEVICES=3 python3 tt3d_export.py \
   --model "dreamfusion-sd" \
   --source-path /media/data2/mconti/TT3D/models/Threestudio-DreamFusionSD/outputs
+
+
+### 
+### FANTASIA-3D
+### 
+
+CUDA_VISIBLE_DEVICES=3 python3 tt3d_generate.py \
+  --model "fantasia3d" \
+  --prompt-file /media/data2/mconti/TT3D/prompts/test.v1.n2.txt \
+  --out-path /media/data2/mconti/TT3D/models/Threestudio-Fantasia3D/outputs \
+  --train-steps=100
+
+CUDA_VISIBLE_DEVICES=3 python3 tt3d_export.py \
+  --model "fantasia3d" \
+  --source-path /media/data2/mconti/TT3D/models/Threestudio-Fantasia3D/outputs
