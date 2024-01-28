23b8512d9a5e6c9261ee71366c1da0f736c94e19
Matteo Conti <contimatteo1505@gmail.com>
2024-01-28 12:46:17 +0100

    WIP

--- run.sh
+++ run.sh
@@ -30,3 +30,18 @@ CUDA_VISIBLE_DEVICES=3 python3 tt3d_generate.py \
 CUDA_VISIBLE_DEVICES=3 python3 tt3d_export.py \
   --model "fantasia3d" \
   --source-path /media/data2/mconti/TT3D/models/Threestudio-Fantasia3D/outputs
+
+
+### 
+### PROFILIC-DREAMER
+### 
+
+CUDA_VISIBLE_DEVICES=3 python3 tt3d_generate.py \
+  --model "prolificdreamer" \
+  --prompt-file /media/data2/mconti/TT3D/prompts/test.v1.n2.txt \
+  --out-path /media/data2/mconti/TT3D/models/Threestudio-ProlificDreamer/outputs \
+  --train-steps=100
+
+CUDA_VISIBLE_DEVICES=3 python3 tt3d_export.py \
+  --model "prolificdreamer" \
+  --source-path /media/data2/mconti/TT3D/models/Threestudio-ProlificDreamer/outputs
--- tt3d_generate.py
+++ tt3d_generate.py
@@ -197,11 +197,18 @@ def main(model: str, prompt_filepath: Path, out_rootpath: Path, train_steps: int
     assert len(model) > 0
     assert model in Utils.Configs.MODELS_SUPPORTED
     assert isinstance(out_rootpath, Path)
-    assert out_rootpath.exists()
-    assert out_rootpath.is_dir()
+    # assert out_rootpath.exists()
+    # assert out_rootpath.is_dir()
     assert isinstance(train_steps, int)
     assert train_steps > 0
     assert isinstance(skip_existing, bool)
+
+    if out_rootpath.exists():
+        assert out_rootpath.is_dir()
+    else:
+        out_rootpath.mkdir(parents=True)
+
+    #
 
     prompts = Utils.Prompt.extract_from_file(filepath=prompt_filepath)
 
@@ -225,23 +232,6 @@ def main(model: str, prompt_filepath: Path, out_rootpath: Path, train_steps: int
             out_rootpath=out_rootpath,
             train_steps=train_steps,
         )
-
-        # args_configs: List[Tuple[dict, list]] = None
-        # if model == "dreamfusion-sd":
-        #     # __dreamfusionsd(
-        #     #     prompt=prompt, out_rootpath=out_rootpath, train_steps=train_steps,
-        #     # )
-        #     args_configs = Utils.Models.dreamfusionsd(
-        #         args_builder_fn=_build_default_args,
-        #         prompt=prompt,
-        #         out_rootpath=out_rootpath,
-        #         train_steps=train_steps,
-        #     )
-        # if args_configs is None:
-        #     raise Exception("Model is supported but still not implemented.")
-        # for args_config in args_configs:
-        #     run_args, run_extra_args = args_config
-        #     run_launch_script(run_args=run_args, run_extra_args=run_extra_args)
 
 
 ###
