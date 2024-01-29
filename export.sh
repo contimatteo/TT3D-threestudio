2bb0f996729e4a082acd0056c3804a46005c24cd
Matteo Conti <contimatteo1505@gmail.com>
2024-01-29 22:46:33 +0100

    WIP

---
+++ export.sh
@@ -0,0 +1,56 @@
+###
+
+exit 0
+
+### 
+### DREAMFUSION
+### 
+
+CUDA_VISIBLE_DEVICES=3 python3 tt3d_export.py \
+  --model "dreamfusion-sd" \
+  --source-path /media/data2/mconti/TT3D/models/Threestudio-DreamFusionSD/outputs
+
+
+### 
+### FANTASIA-3D
+### 
+
+CUDA_VISIBLE_DEVICES=3 python3 tt3d_export.py \
+  --model "fantasia3d" \
+  --source-path /media/data2/mconti/TT3D/models/Threestudio-Fantasia3D/outputs
+
+
+### 
+### PROFILIC-DREAMER
+### 
+
+CUDA_VISIBLE_DEVICES=3 python3 tt3d_export.py \
+  --model "prolificdreamer" \
+  --source-path /media/data2/mconti/TT3D/models/Threestudio-ProlificDreamer/outputs
+
+
+### 
+### MAGIC-3D
+### 
+
+CUDA_VISIBLE_DEVICES=3 python3 tt3d_export.py \
+  --model "magic3d" \
+  --source-path /media/data2/mconti/TT3D/models/Threestudio-Magic3D/outputs
+
+
+### 
+### TEXT-MESH
+### 
+
+CUDA_VISIBLE_DEVICES=3 python3 tt3d_export.py \
+  --model "textmesh" \
+  --source-path /media/data2/mconti/TT3D/models/Threestudio-TextMesh/outputs
+
+
+### 
+### HIFA
+### 
+
+CUDA_VISIBLE_DEVICES=3 python3 tt3d_export.py \
+  --model "hifa" \
+  --source-path /media/data2/mconti/TT3D/models/Threestudio-HiFA/outputs
--- generate.sh
+++ generate.sh
@@ -12,10 +12,6 @@ CUDA_VISIBLE_DEVICES=3 python3 tt3d_generate.py \
   --out-path /media/data2/mconti/TT3D/models/Threestudio-DreamFusionSD/outputs \
   --train-steps=100
 
-CUDA_VISIBLE_DEVICES=3 python3 tt3d_export.py \
-  --model "dreamfusion-sd" \
-  --source-path /media/data2/mconti/TT3D/models/Threestudio-DreamFusionSD/outputs
-
 
 ### 
 ### FANTASIA-3D
@@ -26,10 +22,6 @@ CUDA_VISIBLE_DEVICES=3 python3 tt3d_generate.py \
   --prompt-file /media/data2/mconti/TT3D/prompts/test.v1.n2.txt \
   --out-path /media/data2/mconti/TT3D/models/Threestudio-Fantasia3D/outputs \
   --train-steps=100
-
-CUDA_VISIBLE_DEVICES=3 python3 tt3d_export.py \
-  --model "fantasia3d" \
-  --source-path /media/data2/mconti/TT3D/models/Threestudio-Fantasia3D/outputs
 
 
 ### 
@@ -42,10 +34,6 @@ CUDA_VISIBLE_DEVICES=3 python3 tt3d_generate.py \
   --out-path /media/data2/mconti/TT3D/models/Threestudio-ProlificDreamer/outputs \
   --train-steps=100
 
-CUDA_VISIBLE_DEVICES=3 python3 tt3d_export.py \
-  --model "prolificdreamer" \
-  --source-path /media/data2/mconti/TT3D/models/Threestudio-ProlificDreamer/outputs
-
 
 ### 
 ### MAGIC-3D
@@ -56,10 +44,6 @@ CUDA_VISIBLE_DEVICES=3 python3 tt3d_generate.py \
   --prompt-file /media/data2/mconti/TT3D/prompts/test.v1.n2.txt \
   --out-path /media/data2/mconti/TT3D/models/Threestudio-Magic3D/outputs \
   --train-steps=100
-
-CUDA_VISIBLE_DEVICES=3 python3 tt3d_export.py \
-  --model "magic3d" \
-  --source-path /media/data2/mconti/TT3D/models/Threestudio-Magic3D/outputs
 
 
 ### 
@@ -72,10 +56,6 @@ CUDA_VISIBLE_DEVICES=3 python3 tt3d_generate.py \
   --out-path /media/data2/mconti/TT3D/models/Threestudio-TextMesh/outputs \
   --train-steps=100
 
-CUDA_VISIBLE_DEVICES=3 python3 tt3d_export.py \
-  --model "textmesh" \
-  --source-path /media/data2/mconti/TT3D/models/Threestudio-TextMesh/outputs
-
 
 ### 
 ### HIFA
@@ -86,7 +66,3 @@ CUDA_VISIBLE_DEVICES=3 python3 tt3d_generate.py \
   --prompt-file /media/data2/mconti/TT3D/prompts/test.v1.n2.txt \
   --out-path /media/data2/mconti/TT3D/models/Threestudio-HiFA/outputs \
   --train-steps=100
-
-CUDA_VISIBLE_DEVICES=3 python3 tt3d_export.py \
-  --model "hifa" \
-  --source-path /media/data2/mconti/TT3D/models/Threestudio-HiFA/outputs
