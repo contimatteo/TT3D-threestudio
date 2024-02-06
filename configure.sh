25f423752c0d920a373db27af6a29f13e4f70016
Matteo Conti <contimatteo1505@gmail.com>
2024-02-06 17:49:28 +0100

    WIP

--- configure.sh
+++ configure.sh
@@ -1,16 +1,17 @@
 ###
 
-exit 0
+# exit 0
 
 ROOT_DIR="/media/data2/mconti/TT3D"
 PROMPT_DIR="${ROOT_DIR}/prompts"
-OUT_DIR="${ROOT_DIR}/outputs"
+OUT_DIR="${ROOT_DIR}/outputs/configuration"
 
-GPU=1
-PROMPT_FILE="${PROMPT_DIR}/test.t3bench.n10.txt"
+GPU=0
+PROMPT_FILE="${PROMPT_DIR}/test.t3bench.n1.txt"
 
-export HF_HUB_OFFLINE=1
+export HF_HUB_OFFLINE=0
 export HF_HOME="${ROOT_DIR}/checkpoints/huggingface"
+export HF_HUB_DOWNLOAD_TIMEOUT=30
 
 
 ### 
@@ -21,7 +22,7 @@ CUDA_VISIBLE_DEVICES=${GPU} python3 tt3d_generate.py \
   --model "dreamfusion-sd" \
   --prompt-file $PROMPT_FILE \
   --out-path "${OUT_DIR}/Threestudio-DreamFusionSD/" \
-  --train-steps="1000"
+  --train-steps="100"
 
 
 ### 
@@ -32,7 +33,7 @@ CUDA_VISIBLE_DEVICES=${GPU} python3 tt3d_generate.py \
   --model "fantasia3d" \
   --prompt-file $PROMPT_FILE \
   --out-path "${OUT_DIR}/Threestudio-Fantasia3D/" \
-  --train-steps="900,100"
+  --train-steps="100,100"
 
 
 ### 
@@ -43,7 +44,7 @@ CUDA_VISIBLE_DEVICES=${GPU} python3 tt3d_generate.py \
   --model "prolificdreamer" \
   --prompt-file $PROMPT_FILE \
   --out-path "${OUT_DIR}/Threestudio-ProlificDreamer/" \
-  --train-steps="800,100,100"
+  --train-steps="100,100,100"
 
 
 ### 
@@ -54,7 +55,7 @@ CUDA_VISIBLE_DEVICES=${GPU} python3 tt3d_generate.py \
   --model "magic3d" \
   --prompt-file $PROMPT_FILE \
   --out-path "${OUT_DIR}/Threestudio-Magic3D/" \
-  --train-steps="900,100"
+  --train-steps="100,100"
 
 
 ### 
@@ -65,7 +66,7 @@ CUDA_VISIBLE_DEVICES=${GPU} python3 tt3d_generate.py \
   --model "textmesh" \
   --prompt-file $PROMPT_FILE \
   --out-path "${OUT_DIR}/Threestudio-TextMesh/" \
-  --train-steps="1000"
+  --train-steps="100"
 
 
 ### 
@@ -76,4 +77,10 @@ CUDA_VISIBLE_DEVICES=${GPU} python3 tt3d_generate.py \
   --model "hifa" \
   --prompt-file $PROMPT_FILE \
   --out-path "${OUT_DIR}/Threestudio-HiFA/" \
-  --train-steps="1000"
+  --train-steps="100"
+
+
+###
+
+# remove generated samples (they are out-of-scope ...)
+rm -rf $OUT_DIR
